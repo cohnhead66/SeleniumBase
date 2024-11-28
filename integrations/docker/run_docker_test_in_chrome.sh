b6954b00987fb910cc8ab6f1b606d07e67415b38
cohnhead66 <60977702+cohnhead66@users.noreply.github.com>
2024-11-27 20:16:17 -0800

    Change test to real

--- integrations/docker/run_docker_test_in_chrome.sh
+++ integrations/docker/run_docker_test_in_chrome.sh
@@ -3,6 +3,6 @@ set -e
 # Run example test from inside Docker image
 echo "Running example SeleniumBase test from Docker with headless Chrome..."
 cd /SeleniumBase/examples/
-/usr/bin/python3 tee-time-test.py
+/usr/bin/python3 tee-time-test.py -r
 which python3
 exec "$@"
