4d67006c8d1f55da02c47b64315818ceae16d3d9
cohnhead66 <60977702+cohnhead66@users.noreply.github.com>
2024-11-27 17:47:30 -0800

    change to raw albertsons.py

--- integrations/docker/run_docker_test_in_chrome.sh
+++ integrations/docker/run_docker_test_in_chrome.sh
@@ -3,6 +3,6 @@ set -e
 # Run example test from inside Docker image
 echo "Running example SeleniumBase test from Docker with headless Chrome..."
 cd /SeleniumBase/examples/cdp_mode/
-/usr/bin/python3 raw_cdp.py
+/usr/bin/python3 raw_albertsons.py
 which python3
 exec "$@"
