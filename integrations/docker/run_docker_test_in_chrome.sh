9d10a25ed674fb9a083c71793d1310f84a99b9a0
cohnhead66 <60977702+cohnhead66@users.noreply.github.com>
2024-11-27 16:43:23 -0800

    change pytest to pyton3

--- integrations/docker/run_docker_test_in_chrome.sh
+++ integrations/docker/run_docker_test_in_chrome.sh
@@ -2,6 +2,6 @@
 set -e
 # Run example test from inside Docker image
 echo "Running example SeleniumBase test from Docker with headless Chrome..."
-cd /SeleniumBase/examples/ && pytest my_first_test.py --browser=chrome --headless
+cd /SeleniumBase/examples/ && usr/bin/python3 my_first_test.py --browser=chrome --headless
 which python3
 exec "$@"
