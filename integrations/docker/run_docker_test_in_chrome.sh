f9c1f4507a17d31d7c35c2e6471420aeace9303b
cohnhead66 <60977702+cohnhead66@users.noreply.github.com>
2024-11-27 17:00:58 -0800

    update bash commands

--- integrations/docker/run_docker_test_in_chrome.sh
+++ integrations/docker/run_docker_test_in_chrome.sh
@@ -2,6 +2,9 @@
 set -e
 # Run example test from inside Docker image
 echo "Running example SeleniumBase test from Docker with headless Chrome..."
-cd /SeleniumBase/examples/ && usr/bin/python3 my_first_test.py --browser=chrome --headless
+ls
+cd /SeleniumBase/examples/
+ls
+/usr/bin/python3 my_first_test.py
 which python3
 exec "$@"
