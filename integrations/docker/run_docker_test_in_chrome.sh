231a76dfd63ca10ee02997edfa7f0361d895ec16
cohnhead66 <60977702+cohnhead66@users.noreply.github.com>
2024-11-27 17:31:15 -0800

    fix bash

--- integrations/docker/run_docker_test_in_chrome.sh
+++ integrations/docker/run_docker_test_in_chrome.sh
@@ -2,7 +2,6 @@
 set -e
 # Run example test from inside Docker image
 echo "Running example SeleniumBase test from Docker with headless Chrome..."
-ls
 cd /SeleniumBase/examples/cdp_mode/
 /usr/bin/python3 raw_cdp.py
 which python3
